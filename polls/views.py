ab4c361da2a1239f9e177abbd486e45bc571c410
Anton Perekhozhuk <aperekhozhuk@heliostech.fr>
2019-11-30 16:09:34 +0000

    Replaced hardcoded views

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
 from .models import Question
@@ -13,8 +12,9 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/vote.html', {'question': question})
