d7a0c62ccdccd94d5d9b5d0ad2175829ca627566
Anton Perekhozhuk <aperekhozhuk@heliostech.fr>
2019-12-15 15:42:45 +0000

    Improve views and respective tests:
    * now views don't display future Questions
    * Added tests for views for cases with future questions

--- polls/views.py
+++ polls/views.py
@@ -2,8 +2,13 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
+
+def get_questions():
+    """Excludes any questions that aren't published yet."""
+    return Question.objects.filter(pub_date__lte=timezone.now())
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
@@ -11,17 +16,21 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
-
+        return get_questions().order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        return get_questions()
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+
+    def get_queryset(self):
+        return get_questions()
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
