6d279671c6debdbdd9076d57b16507db1acc234a
Anton Perekhozhuk <aperekhozhuk@heliostech.fr>
2019-12-15 15:02:24 +0000

    Generic views

--- ChordsApp/urls.py
+++ ChordsApp/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
-from polls.views import index as polls_page
+from polls.views import IndexView as polls_page
 
 urlpatterns = [
-    path('', polls_page),
+    path('', polls_page.as_view()),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
