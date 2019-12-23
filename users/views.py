03f8849418bfaa8ca8ff8ae539800f77034a3b3d
Anton Perekhozhuk <aperekhozhuk@heliostech.fr>
2019-12-23 04:48:05 +0000

    Custom Register form

--- users/views.py
+++ users/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
 from django.contrib import messages
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('home-page')
     else:
-        form = UserCreationForm()
-    return render(request, 'users/register.html', {'form': form})
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form.as_p})
