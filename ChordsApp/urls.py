3b1fcfcd02d1ec5fefe8176579cac06c863cf792
Anton Perekhozhuk <aperekhozhuk@heliostech.fr>
2019-12-23 00:37:50 +0000

    Add register page and set respective route

--- ChordsApp/urls.py
+++ ChordsApp/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 from polls.views import IndexView as polls_page
+from users import views as users_views
 
 urlpatterns = [
     path('', polls_page.as_view()),
+    path('register/', users_views.register, name='register'),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
